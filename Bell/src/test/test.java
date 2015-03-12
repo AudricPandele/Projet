e5ab3a64b5bdfad62b36f20a3f8c4ca8fcdf33a8
Audric <audric.pandele@ingesup.com>
2015-03-12 11:23:39 +0100

    first

--- Bell/src/test/test.java
+++ Bell/src/test/test.java
@@ -1,9 +1,10 @@
+package test;
 
-public class Test {
+public class test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Coucou");
+		System.out.println("coucou");
 	}
 
 }
