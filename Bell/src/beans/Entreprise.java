826092c50882f521cfdcf913717958130c615339
louischarbonier <louis.charbonier@y-nov.com>
2015-03-19 12:19:35 +0100

    class Entreprise et fjizeo

--- Bell/src/beans/Entreprise.java
+++ Bell/src/beans/Entreprise.java
@@ -2,4 +2,63 @@ package beans;
 
 public class Entreprise {
 
+	private String nom;
+	private String siret;
+	private String addresse;
+	private int telephone;
+	
+	
+	//Constructeur
+	public Entreprise(String nom, String siret, String addresse, int telephone) {
+		this.nom = nom;
+		this.siret = siret;
+		this.addresse = addresse;
+		this.telephone = telephone;
+	}
+
+
+	public String getNom() {
+		return nom;
+	}
+
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+
+	public String getSiret() {
+		return siret;
+	}
+
+
+	public void setSiret(String siret) {
+		this.siret = siret;
+	}
+
+
+	public String getAddresse() {
+		return addresse;
+	}
+
+
+	public void setAddresse(String addresse) {
+		this.addresse = addresse;
+	}
+
+
+	public int getTelephone() {
+		return telephone;
+	}
+
+
+	public void setTelephone(int telephone) {
+		this.telephone = telephone;
+	}
+	
+	
+	
+	//TODO DeleteEntreprise(){ }
+	
+	
 }
--- Bell/src/beans/Tache.java
+++ Bell/src/beans/Tache.java
@@ -1,5 +1,53 @@
 package beans;
 
 public class Tache {
+	
+	private String nom;
+	private float prixUnitaire;
+	private int quantite;
+	private float prixHT;
+	
+	//Constructeur
+	public Tache(String nom, float prixUnitaire, int quantite, float prixHT) {
+		this.nom = nom;
+		this.prixUnitaire = prixUnitaire;
+		this.quantite = quantite;
+		this.prixHT = prixHT;
+	}
+	//GET - SET
+	public String getNom() {
+		return nom;
+	}
 
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public float getPrixUnitaire() {
+		return prixUnitaire;
+	}
+
+	public void setPrixUnitaire(float prixUnitaire) {
+		this.prixUnitaire = prixUnitaire;
+	}
+
+	public int getQuantite() {
+		return quantite;
+	}
+
+	public void setQuantite(int quantite) {
+		this.quantite = quantite;
+	}
+
+	public float getPrixHT() {
+		return prixHT;
+	}
+
+	public void setPrixHT(float prixHT) {
+		this.prixHT = prixHT;
+	}
+	
+	//TODO DeleteTache()
+	//TODO CalculPrixHT()
+	
 }
