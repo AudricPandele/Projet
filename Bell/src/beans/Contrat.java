ceffc3b4afaa440ec4d11abace793fd4cd998451
Auguste Laulhau <aug.laulhau@gmail.com>
2015-03-19 12:23:43 +0100

    Classe Contrat, Facture

--- Bell/src/beans/Contrat.java
+++ Bell/src/beans/Contrat.java
@@ -1,5 +1,61 @@
 package beans;
 
-public class Contrat {
+import java.util.Date;
 
+public class Contrat {
+	
+	private int id;
+	private String fournisseur;
+	private String client;
+	private String titre;
+	private float prix;
+	private Date date;
+	
+	public Contrat(int id, String fournisseur, String client, String titre,
+					float prix, Date date) 
+	{
+		this.id = id;
+		this.fournisseur = fournisseur;
+		this.client = client;
+		this.titre = titre;
+		this.prix = prix;
+		this.date = date;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getFournisseur() {
+		return fournisseur;
+	}
+	public void setFournisseur(String fournisseur) {
+		this.fournisseur = fournisseur;
+	}
+	public String getClient() {
+		return client;
+	}
+	public void setClient(String client) {
+		this.client = client;
+	}
+	public String getTitre() {
+		return titre;
+	}
+	public void setTitre(String titre) {
+		this.titre = titre;
+	}
+	public float getPrix() {
+		return prix;
+	}
+	public void setPrix(float prix) {
+		this.prix = prix;
+	}
+	public Date getDate() {
+		return date;
+	}
+	public void setDate(Date date) {
+		this.date = date;
+	} 
 }
--- Bell/src/beans/Facture.java
+++ Bell/src/beans/Facture.java
@@ -1,5 +1,53 @@
 package beans;
 
 public class Facture {
+	
+	private int id;
+	private String fournisseur;
+	private String client;
+	private boolean estPaye;
+	
+	public Facture(int id, String fournisseur, String client, boolean estPaye) 
+	{
+		this.id = id;
+		this.fournisseur = fournisseur;
+		this.client = client;
+		this.estPaye = estPaye;
+	}
 
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getFournisseur() {
+		return fournisseur;
+	}
+
+	public void setFournisseur(String fournisseur) {
+		this.fournisseur = fournisseur;
+	}
+
+	public String getClient() {
+		return client;
+	}
+
+	public void setClient(String client) {
+		this.client = client;
+	}
+
+	public boolean isEstPaye() {
+		return estPaye;
+	}
+
+	public void setEstPaye(boolean estPaye) {
+		this.estPaye = estPaye;
+	}
+	
+	
+	
+	
 }
