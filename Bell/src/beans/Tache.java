955ba894a6c017f4a5089526c1d12e6510489a19
louischarbonier <louis.charbonier@y-nov.com>
2015-04-23 09:03:22 +0200

    pour pull

--- Bell/src/beans/Tache.java
+++ Bell/src/beans/Tache.java
@@ -1,5 +1,14 @@
 package beans;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import Connection.*;
+
+import com.mysql.jdbc.Statement;
+
 public class Tache {
 	
 	private String nom;
@@ -45,6 +54,87 @@ public class Tache {
 
 	public void setPrixHT(float prixHT) {
 		this.prixHT = prixHT;
+	}
+	
+	public static ArrayList selectAllTacheNom() throws SQLException, ClassNotFoundException
+	{
+		ArrayList allTacheNom = new ArrayList();
+		Conn conn = new Conn();
+		Connection connection = conn.getConnect();
+
+		Statement statement = (Statement) connection.createStatement();
+		ResultSet resultat = statement.executeQuery( "SELECT *  FROM tache;" );
+
+		while ( resultat.next() ) {
+		    String nom = resultat.getString( "nom" );
+		    allTacheNom.add(nom);
+		    return allTacheNom;
+		}
+		return allTacheNom;
+	}
+	public static ArrayList selectAllTachePrix() throws SQLException, ClassNotFoundException
+	{
+		ArrayList allTachePrix = new ArrayList();
+		Conn conn = new Conn();
+		Connection connection = conn.getConnect();
+
+		Statement statement = (Statement) connection.createStatement();
+		ResultSet resultat = statement.executeQuery( "SELECT *  FROM tache;" );
+
+		while ( resultat.next() ) {
+		    float prix = resultat.getInt("prix");
+		    allTachePrix.add(prix);
+		    return allTachePrix;
+		}
+		return allTachePrix;
+	}
+	public static ArrayList selectAllTacheQuantite() throws SQLException, ClassNotFoundException
+	{
+		ArrayList allTacheQuantite = new ArrayList();
+		Conn conn = new Conn();
+		Connection connection = conn.getConnect();
+
+		Statement statement = (Statement) connection.createStatement();
+		ResultSet resultat = statement.executeQuery( "SELECT *  FROM tache;" );
+
+		while ( resultat.next() ) {
+		    int quantite = resultat.getInt("quantite");
+		    allTacheQuantite.add(quantite);
+		    return allTacheQuantite;
+		}
+		return allTacheQuantite;
+	}
+	public static ArrayList selectAllTachePrixHT() throws SQLException, ClassNotFoundException
+	{
+		ArrayList allTachePrixHT = new ArrayList();
+		Conn conn = new Conn();
+		Connection connection = conn.getConnect();
+
+		Statement statement = (Statement) connection.createStatement();
+		ResultSet resultat = statement.executeQuery( "SELECT *  FROM tache;" );
+
+		while ( resultat.next() ) {
+		    float prixHT = resultat.getInt("prixHT");
+		    allTachePrixHT.add(prixHT);
+		    return allTachePrixHT;
+		}
+		return allTachePrixHT;
+	}
+	public static ArrayList selectAllTache() throws SQLException, ClassNotFoundException
+	{
+		ArrayList allTache = new ArrayList();
+		Conn conn = new Conn();
+		Connection connection = conn.getConnect();
+
+		Statement statement = (Statement) connection.createStatement();
+		ResultSet resultat = statement.executeQuery( "SELECT *  FROM tache;" );
+
+		while ( resultat.next() ) {
+		    float prixHT = resultat.getInt("prixHT");
+		    allTache.add(prixHT);
+		    return allTache;
+		}
+		return allTache;
 	}
 	
 	//TODO DeleteTache()
--- Bell/src/launcher/LauncherApp.java
+++ Bell/src/launcher/LauncherApp.java
@@ -1,15 +1,21 @@
 package launcher;
+import java.sql.SQLException;
+
 import javax.swing.JFrame;
 
+import beans.*;
 import graphique.*;
+import Connection.*;
 
 public class LauncherApp {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ClassNotFoundException, SQLException {
 		// instanciation d'un objet MaFrame
 		Frame frame = new Frame();
 		// action a adopter quand on ferme la fenetre
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setVisible(true);
+		
+		
 		
 	}
 }
